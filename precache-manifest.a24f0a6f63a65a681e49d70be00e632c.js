ad2c74d377d3b7f2e9ae899849599d9f1c7355e1
vineetk13 <vineetkme.99@gmail.com>
2020-11-13 00:11:25 +0530

    Updates

---
+++ precache-manifest.a24f0a6f63a65a681e49d70be00e632c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f7fcd9755c8996dbca21ed87504fac",
+    "url": "/react-image-gallery/index.html"
+  },
+  {
+    "revision": "a24f01af00ed9b2261eb",
+    "url": "/react-image-gallery/static/css/main.8a4c623a.chunk.css"
+  },
+  {
+    "revision": "b63fd8b35fd1da3c405c",
+    "url": "/react-image-gallery/static/js/2.8ab267e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-gallery/static/js/2.8ab267e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24f01af00ed9b2261eb",
+    "url": "/react-image-gallery/static/js/main.528da7f2.chunk.js"
+  },
+  {
+    "revision": "11ad39eeb649f02fa41d",
+    "url": "/react-image-gallery/static/js/runtime-main.e39fd300.js"
+  }
+]);
